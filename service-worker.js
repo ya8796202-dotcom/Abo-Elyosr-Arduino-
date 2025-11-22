2826648fa8c627fcdcb483e16a09689b986f15bf
ya8796202-dotcom <ya8796202@gmail.com>
2025-11-22 09:24:19 +0200

    Update service-worker.js
    Signed-off-by: ya8796202-dotcom <ya8796202@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -9,3 +9,49 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
   console.log("Fetching:", event.request.url);
 });
+
+
+
+const CACHE_NAME = "my-cache-v1";
+const urlsToCache = [
+  "/",              // الصفحة الرئيسية
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// أثناء التثبيت: نخزن الملفات في الكاش
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// أثناء التفعيل: نمسح أي كاش قديم
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// أثناء الطلبات: نرد من الكاش أو من الشبكة
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
